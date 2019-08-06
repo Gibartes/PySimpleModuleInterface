0fa28f563ceeed240df9f83e88e60029420a9cad
Gibartes <konm2000@hotmail.com>
2019-08-06 11:05:45 +0900

    ver 1.0.0

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name 		= 'moduleInterface',
+    version 	= '1.0.0',
+    description	= 'Simple Python Module Interface',
+    author		= 'gibartes',
+    install_requires = [],
+    packages    = find_packages(exclude = ['docs','*.c','*.h','makefile']),
+    keyword		= 'interface',
+    python_requires = '>=3',
+    zip_safe    = False,
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
