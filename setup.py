882571253ae8001b652a6780b047282ceda3a7ee
Gibartes <konm2000@hotmail.com>
2019-08-13 15:44:02 +0900

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name 		= 'moduleInterface',
-    version 	= '1.1.0',
+    version 	= '1.1.1',
     description	= 'Simple Python Module Interface',
     author		= 'gibartes',
     install_requires = [],
